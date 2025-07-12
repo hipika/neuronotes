cce952353132018ebf0ff9dfdd8b1bf06f144063
hipika <arnavchoudhary53@gmail.com>
2025-07-11 21:50:33 -0400

    impl. direct cors use

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
-import useCors from "./middleware/init";
-
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 5000;
-
+const url = "http://localhost:5173";
 // Middleware
+
+app.use(cors({origin: url}))
 app.use(express.json())
 app.use("/api/users", require("./routes/userRoutes"))
-useCors("http://localhost:5173");
+
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`)
