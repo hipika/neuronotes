5d92440732fe3f28ca70fa1b7e1c206b9ae4aa6b
hipika <arnavchoudhary53@gmail.com>
2025-07-11 21:49:46 -0400

    removed form tag; added props

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 
-const Form = ({ autocomplete, placeholder, type, id }) => {
+const Form = ({ autocomplete, placeholder, type, id, value, name, onChange }) => {
   return (
     <div>
-        
-    <form className="space-y-6" action="#" method="POST">
-      <div>
-        <div className="mt-2">
-          <input placeholder={placeholder} type={type} id={id} autocomplete={autocomplete} required class="block w-full bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-black sm:text-sm/6" />
-        </div>
-      </div>
 
-    </form>
+          <input placeholder={placeholder} name={name} onChange={onChange} type={type} id={id} autoComplete={autocomplete} value={value} required class="block w-full bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-black sm:text-sm/6" />
+
+
     </div>
   )
 }
