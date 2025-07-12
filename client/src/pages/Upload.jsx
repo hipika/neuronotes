d198ac88eda8e1a41dbd1ec1a59ad3c7e1792b82
hipika <arnavchoudhary53@gmail.com>
2025-07-12 01:24:09 -0400

    added navbar directly into upload page instead of uploader component

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Uploader from '../components/Uploader'
+import { Link } from 'react-router'
 
 const Upload = () => {
   return (
     <div>
+
+      <div className="w-full flex justify-between items-center p-4 fixed top-0 left-0 z-[20]">
+        <button className="text-black hover:bg-gray-200 px-2 py-1 rounded-md cursor-pointer">Saved</button>
+
+        <Link to="/">
+            <button className="text-black hover:bg-gray-200 px-2 py-1 rounded-md cursor-pointer">Logout</button>
+        </Link>
+      </div>
       <Uploader/>
 
     </div>
