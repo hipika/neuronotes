fb56aab2e317ea80329437bec28a541019087b76
hipika <arnavchoudhary53@gmail.com>
2025-07-11 21:49:10 -0400

    connected to REST api; fixed join form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  { BrowserRouter, Route, Routes, Link } from "react-router";
 import Form from '../components/Form';
 import Navbar from '../components/Navbar';
 import Animation from '../components/Animation';
 const Register = () => {
+  const [joinData, setJoinData] = useState({ username: "", email: "", password: "" })
+  // handling change ...-> apply this function to all values
+  const handleChange = (e) => {
+    setJoinData({ ...joinData, [e.target.name]: e.target.value})
+  }
+
+  // handling submit
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch("http://localhost:5000/api/users/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(joinData) // turns updated joinData into json format
+    })
+
+    const data = await response.json()
+    console.log(data);
+  }
+
+
   return (
     <>
       <Navbar />
@@ -19,20 +39,21 @@ const Register = () => {
             </h2>
           </div>
 
-          <div className="mt-10">
-            <Form placeholder="Username" type="text" autocomplete="username" id="username" />
-            <Form placeholder="Email" type="email" autocomplete="email" id="email" />
-            <Form placeholder="Password" type="password" autocomplete="current-password" id="password" />
+          <form className="mt-10 space-y-6" onSubmit={handleSubmit}>
+            <Form placeholder="Username" name="username" onChange={handleChange} type="text" autocomplete="username" id="username" value={joinData.username} />
+            <Form placeholder="Email" name="email" onChange={handleChange} type="email" autocomplete="email" id="email" value={joinData.email} />
+            <Form placeholder="Password" name="password" onChange={handleChange} type="password" autocomplete="current-password" id="password" value={joinData.password}/>
 
             <div className="mt-2">
               <button
                 type="submit"
+                onSubmit={handleSubmit}
                 className="flex w-full justify-center bg-black px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-black/60 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Join
               </button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </>
