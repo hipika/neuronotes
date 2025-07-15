02bc39dbf38a974afc276d60015873a7a57890d3
hipika <arnavchoudhary53@gmail.com>
2025-07-15 18:28:31 -0400

    renamed to className

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -4,7 +4,7 @@ const Form = ({ autocomplete, placeholder, type, id, value, name, onChange }) =>
   return (
     <div>
 
-          <input placeholder={placeholder} name={name} onChange={onChange} type={type} id={id} autoComplete={autocomplete} value={value} required class="block w-full bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-black sm:text-sm/6" />
+          <input placeholder={placeholder} name={name} onChange={onChange} type={type} id={id} autoComplete={autocomplete} value={value} className="block w-full bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-black sm:text-sm/6" />
 
 
     </div>
