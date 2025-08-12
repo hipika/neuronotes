4096e95ff78080ad4c17cd38ed2d4c97b82c1c22
hipika <arnavchoudhary53@gmail.com>
2025-08-11 23:33:50 -0400

    misc comments

--- client/src/components/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes.jsx
@@ -2,9 +2,9 @@ import { Outlet, Navigate } from "react-router";
 
 const ProtectedRoutes = () => {
 
-    
+    // get current token  
     const token = localStorage.getItem('jwt');
-
+    // if exists navigate to /upload else back to login
     return token ? <Outlet /> : <Navigate to="/login"/>
 
 }
